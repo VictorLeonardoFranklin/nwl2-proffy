79f0cd107db98092c87248a940f48fb7eccc1d88
Victor Franklin <VictorLeonardoFranklin@gmail.com>
2020-08-05 21:57:51 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from "react";
+import Landing from "./pages/landing";
 
 import "./assets/styles/global.css";
 
 function App() {
   return (
     <>
-      <div>
-        <h1>hello world</h1>
-      </div>
+      <Landing />
     </>
   );
 }
